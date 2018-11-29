977b620276eee87339fa18b069d604606d1f1722
Varodom Nualplub <varodom.naul@gmail.com>
2018-11-29 10:28:40 +0700

    update code

--- hello-gin.go
+++ hello-gin.go
@@ -7,8 +7,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Msg struct {
+	Name string `json:"name"`
+}
+
 func main() {
 	r := gin.Default()
+	r.POST("/hello", func(c *gin.Context) {
+		var msg Msg
+		if err := c.ShouldBindJSON(&msg); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"message": "error:" + err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, " + msg.Name,
+		})
+	})
+	r.GET("/hello/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, " + name,
+		})
+	})
 	r.GET("/hello", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello, World",
